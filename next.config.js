a4fe7392fdd137690370f0c6bea098d9d4416348
tomasbankauskas <tbdizainas@gmail.com>
2022-01-05 16:01:03 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -15,13 +15,13 @@ module.exports = withBundleAnalyzer({
         // Allow production builds to successfully complete even if your project has ESLint errors.
         ignoreDuringBuilds: true
     },
-    webpack: (config, { webpack, dev }) => {
+    webpack: (config, { dev }) => {
         // Tell webpack to ignore watching content files in the content folder.
         // Otherwise webpack recompiles the app and refreshes the whole page.
         // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
         // function to update the content on the page without refreshing the
         // whole page
-        config.plugins.push(new webpack.WatchIgnorePlugin({ paths: [/\/content\//] }));
+        config.watchOptions.ignored.push('/content/');
         if (dev) {
             // enable tree shaking for development mode, on production it is on by default
             config.optimization.usedExports = true;
