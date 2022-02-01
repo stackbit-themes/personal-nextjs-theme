3f3cdf8d502d7e8062a4ce7a24ee192d22832a7b
TomasBankauskas <tbdizainas@gmail.com>
2022-02-02 00:21:01 +0200

    updated sections

--- src/components/sections/QuoteSection/index.tsx
+++ src/components/sections/QuoteSection/index.tsx
@@ -3,98 +3,43 @@ import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
-import { getDataAttrs } from '../../../utils/get-data-attrs';
+import Section from '../Section';
 
 export default function QuoteSection(props) {
-    const cssId = props.elementId || null;
-    const colors = props.colors || 'colors-a';
-    const styles = props.styles || {};
-    const sectionWidth = styles.self?.width || 'wide';
-    const sectionHeight = styles.self?.height || 'auto';
-    const sectionJustifyContent = styles.self?.justifyContent || 'center';
+    const { type, elementId, colors, quote, name, title, styles = {}, 'data-sb-field-path': fieldPath } = props;
     return (
-        <div
-            id={cssId}
-            {...getDataAttrs(props)}
-            className={classNames(
-                'sb-component',
-                'sb-component-section',
-                'sb-component-quote-section',
-                colors,
-                'flex',
-                'flex-col',
-                'justify-center',
-                'relative',
-                mapMinHeightStyles(sectionHeight),
-                styles.self?.margin,
-                styles.self?.padding || 'py-12 px-4',
-                styles.self?.borderColor,
-                styles.self?.borderStyle ? mapStyles({ borderStyle: styles.self?.borderStyle }) : 'border-none',
-                styles.self?.borderRadius ? mapStyles({ borderRadius: styles.self?.borderRadius }) : null
-            )}
-            style={{
-                borderWidth: styles.self?.borderWidth ? `${styles.self?.borderWidth}px` : null
-            }}
-        >
-            <div className={classNames('flex', 'relative', 'w-full', mapStyles({ justifyContent: sectionJustifyContent }))}>
-                <div className={classNames('w-full', mapMaxWidthStyles(sectionWidth))}>
-                    <blockquote>
-                        {props.quote && (
-                            <Markdown
-                                options={{ forceBlock: true, forceWrapper: true }}
-                                className={classNames(
-                                    'sb-markdown',
-                                    'text-4xl',
-                                    'sm:text-6xl',
-                                    'sm:leading-tight',
-                                    styles.quote ? mapStyles(styles.quote) : null
-                                )}
-                                data-sb-field-path=".quote"
+        <Section type={type} elementId={elementId} colors={colors} styles={styles.self} data-sb-field-path={fieldPath}>
+            <blockquote>
+                {quote && (
+                    <Markdown
+                        options={{ forceBlock: true, forceWrapper: true }}
+                        className={classNames('sb-markdown', 'text-3xl', 'sm:text-5xl', 'sm:leading-tight', styles.quote ? mapStyles(styles.quote) : null)}
+                        data-sb-field-path=".quote"
+                    >
+                        {quote}
+                    </Markdown>
+                )}
+                {(name || title) && (
+                    <footer className="mt-8 sm:mt-10">
+                        {name && (
+                            <span
+                                className={classNames('block', 'text-lg', 'sm:text-xl', styles.name ? mapStyles(styles.name) : null)}
+                                data-sb-field-path=".name"
                             >
-                                {props.quote}
-                            </Markdown>
+                                {name}
+                            </span>
                         )}
-                        {(props.name || props.title) && (
-                            <footer className="mt-8 sm:mt-10">
-                                {props.name && (
-                                    <span className={classNames('block', 'text-lg', styles.name ? mapStyles(styles.name) : null)} data-sb-field-path=".name">
-                                        {props.name}
-                                    </span>
-                                )}
-                                {props.title && (
-                                    <span className={classNames('block', 'text-lg', styles.title ? mapStyles(styles.title) : null)} data-sb-field-path=".title">
-                                        {props.title}
-                                    </span>
-                                )}
-                            </footer>
+                        {title && (
+                            <span
+                                className={classNames('block', 'text-lg', 'sm:text-xl', styles.title ? mapStyles(styles.title) : null)}
+                                data-sb-field-path=".title"
+                            >
+                                {title}
+                            </span>
                         )}
-                    </blockquote>
-                </div>
-            </div>
-        </div>
+                    </footer>
+                )}
+            </blockquote>
+        </Section>
     );
-}
-
-function mapMinHeightStyles(height) {
-    switch (height) {
-        case 'auto':
-            return 'min-h-0';
-        case 'screen':
-            return 'min-h-screen';
-        default:
-            return null;
-    }
-}
-
-function mapMaxWidthStyles(width) {
-    switch (width) {
-        case 'narrow':
-            return 'max-w-5xl';
-        case 'wide':
-            return 'max-w-7xl';
-        case 'full':
-            return 'max-w-full';
-        default:
-            return null;
-    }
 }
