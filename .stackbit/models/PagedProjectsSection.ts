407047eedf45fb4ed5f04b505b7e167597402dfc
tomasbankauskas <tbdizainas@gmail.com>
2023-03-20 16:17:13 +0200

    updated models

--- .stackbit/models/PagedProjectsSection.ts
+++ .stackbit/models/PagedProjectsSection.ts
@@ -1,32 +1,195 @@
 import { Model } from '@stackbit/types';
 
 export const PagedProjectsSection: Model = {
-  type: "object",
-  name: "PagedProjectsSection",
-  label: "Project feed",
-  labelField: "title",
-  extends: [
-    "ProjectFeedSection"
-  ],
-  fields: [
-    {
-      name: "variant",
-      default: "variant-a"
-    },
-    {
-      name: "title",
-      hidden: true,
-      default: null
-    },
-    {
-      name: "subtitle",
-      hidden: true,
-      default: null
-    },
-    {
-      name: "actions",
-      hidden: true,
-      default: []
-    }
-  ]
-}
+    type: 'object',
+    name: 'PagedProjectsSection',
+    label: 'Project feed',
+    labelField: 'entryTitle',
+    fieldGroups: [
+        {
+            name: 'styles',
+            label: 'Styles',
+            icon: 'palette'
+        },
+        {
+            name: 'settings',
+            label: 'Settings',
+            icon: 'gear'
+        }
+    ],
+    fields: [
+        {
+            type: 'string',
+            name: 'entryTitle',
+            label: 'Title',
+            description: 'The value of the field is used for presentation purposes in Stackbit',
+            default: 'Projects'
+        },
+        {
+            type: 'enum',
+            name: 'colors',
+            label: 'Colors',
+            description: 'The color theme of the section',
+            group: 'styles',
+            controlType: 'palette',
+            options: [
+                {
+                    label: 'Colors A',
+                    value: 'colors-a',
+                    textColor: '$onDark',
+                    backgroundColor: '$dark',
+                    borderColor: '#ececec'
+                },
+                {
+                    label: 'Colors B',
+                    value: 'colors-b',
+                    textColor: '$onLight',
+                    backgroundColor: '$light',
+                    borderColor: '#ececec'
+                },
+                {
+                    label: 'Colors C',
+                    value: 'colors-c',
+                    textColor: '$onPrimary',
+                    backgroundColor: '$primary',
+                    borderColor: '#ececec'
+                },
+                {
+                    label: 'Colors D',
+                    value: 'colors-d',
+                    textColor: '$onSecondary',
+                    backgroundColor: '$secondary',
+                    borderColor: '#ececec'
+                },
+                {
+                    label: 'Colors E',
+                    value: 'colors-e',
+                    textColor: '$onComplementary',
+                    backgroundColor: '$complementary',
+                    borderColor: '#ececec'
+                },
+                {
+                    label: 'Colors F',
+                    value: 'colors-f',
+                    textColor: '$onLight',
+                    backgroundColor: 'transparent',
+                    borderColor: '#ececec'
+                }
+            ],
+            default: 'colors-f'
+        },
+        {
+            type: 'enum',
+            name: 'variant',
+            group: 'styles',
+            label: 'Arrangement',
+            options: [
+                {
+                    label: 'Two columns grid',
+                    value: 'variant-a'
+                },
+                {
+                    label: 'Three columns grid',
+                    value: 'variant-b'
+                },
+                {
+                    label: 'List',
+                    value: 'variant-c'
+                },
+                {
+                    label: 'List alt',
+                    value: 'variant-d'
+                }
+            ],
+            default: 'variant-a',
+            required: true
+        },
+        {
+            type: 'string',
+            name: 'elementId',
+            group: 'settings',
+            label: 'Element ID',
+            description: 'The unique ID for an HTML element, must not contain whitespace',
+            default: ''
+        },
+        {
+            type: 'boolean',
+            name: 'showDate',
+            group: 'settings',
+            label: 'Show project date',
+            default: false
+        },
+        {
+            type: 'boolean',
+            name: 'showDescription',
+            group: 'settings',
+            label: 'Show project description',
+            default: false
+        },
+        {
+            type: 'boolean',
+            name: 'showFeaturedImage',
+            group: 'settings',
+            label: 'Show featured image',
+            default: false
+        },
+        {
+            type: 'boolean',
+            name: 'showReadMoreLink',
+            group: 'settings',
+            label: 'Show keep reading link',
+            default: false
+        },
+        {
+            type: 'style',
+            name: 'styles',
+            styles: {
+                self: {
+                    height: ['auto', 'screen'],
+                    width: ['narrow', 'wide', 'full'],
+                    margin: ['tw0:96'],
+                    padding: ['tw0:96'],
+                    justifyContent: ['flex-start', 'flex-end', 'center'],
+                    borderRadius: '*',
+                    borderWidth: ['0:8'],
+                    borderStyle: '*',
+                    borderColor: [
+                        {
+                            value: 'border-primary',
+                            label: 'Primary color',
+                            color: '$primary'
+                        },
+                        {
+                            value: 'border-secondary',
+                            label: 'Secondary color',
+                            color: '$secondary'
+                        },
+                        {
+                            value: 'border-dark',
+                            label: 'Dark color',
+                            color: '$dark'
+                        },
+                        {
+                            value: 'border-complementary',
+                            label: 'Complementary color',
+                            color: '$complementary'
+                        }
+                    ]
+                }
+            },
+            default: {
+                self: {
+                    height: 'auto',
+                    width: 'wide',
+                    margin: ['mt-0', 'mb-0', 'ml-0', 'mr-0'],
+                    padding: ['pt-12', 'pb-12', 'pl-4', 'pr-4'],
+                    justifyContent: 'center',
+                    borderRadius: 'none',
+                    borderWidth: 0,
+                    borderStyle: 'none',
+                    borderColor: 'border-dark'
+                }
+            }
+        }
+    ]
+};
